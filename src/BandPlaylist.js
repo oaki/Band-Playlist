3883c202fdaa6981500e7deffb65d0a0960642be
pavolbincik <pavolbincik@gmail.com>
2015-08-25 09:32:12 +0200

    add ten round

--- src/BandPlaylist.js
+++ src/BandPlaylist.js
@@ -24,7 +24,7 @@ app.config(['$translateProvider', function ($translateProvider) {
 }]);
 
 app.value('fbURL', 'https://scorching-inferno-4935.firebaseio.com/');
-app.value('rounds', [1, 2, 3, 4, 5, 6, 7, 8, 9]);
+app.value('rounds', [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
 
 /* use strict */
 app.controller('AddSongCtrl', [

0d415c73a6440bca519f525be54669c09d6d8c05
pavolbincik <pavolbincik@gmail.com>
2015-09-07 12:45:55 +0200

    change round

--- src/resources/js/config/values.js
+++ src/resources/js/config/values.js
@@ -1,2 +1,2 @@
 app.value('fbURL', 'https://scorching-inferno-4935.firebaseio.com/');
-app.value('rounds', [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+app.value('rounds', [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]);
